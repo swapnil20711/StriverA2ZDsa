53260daa4482ff9f59ed538b57ba0a505f88a14a
swapnil20711 <swapnil.bhojwani@gmail.com>
2025-01-02 23:32:37 +0530

    second largest element optimal approach

--- arrays/SecondLargestElementInAnArray.kt
+++ arrays/SecondLargestElementInAnArray.kt
@@ -1,5 +1,8 @@
 package arrays
 
+/*
+* TC O(2N)
+* */
 fun findSecondLargestElementInAnArray(nums: IntArray): Int {
     val largestElementInAnArray = findLargestElementInAnArrayBruteForce(nums);
     var secondLargestElementInAnArray = -1;
@@ -11,7 +14,23 @@ fun findSecondLargestElementInAnArray(nums: IntArray): Int {
     return secondLargestElementInAnArray
 }
 
+/*
+* O(N)*/
+fun findSecondLargestElementInArrayOptimal(nums: IntArray): Int {
+    var largestElement = nums[0]
+    var secondLargestElement = -1
+    for (num in nums) {
+        if (num > largestElement) {
+            secondLargestElement = largestElement;
+            largestElement = num;
+        } else if (num < largestElement && num > secondLargestElement) {
+            secondLargestElement = num
+        }
+    }
+    return secondLargestElement;
+}
+
 fun main() {
     val nums = intArrayOf(1, 2, 4, 5, 7, 7)
-    println("Second largest element in the array is ${findSecondLargestElementInAnArray(nums)}")
+    println("Second largest element in the array is ${findSecondLargestElementInArrayOptimal(nums)}")
 }
