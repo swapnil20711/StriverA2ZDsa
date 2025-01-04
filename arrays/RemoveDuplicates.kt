2dd7126865e08ec013fb04d0804150ec550016f0
swapnil20711 <swapnil.bhojwani@gmail.com>
2025-01-04 16:23:12 +0530

    remove duplicates from array optimal approach done

--- arrays/RemoveDuplicates.kt
+++ arrays/RemoveDuplicates.kt
@@ -1,23 +1,36 @@
 package arrays
 
-fun removeDuplicates(nums:Array<Int>): IntArray {
+fun removeDuplicates(nums: Array<Int>): IntArray {
     val set = mutableSetOf<Int>()
-    for (num in nums){
+    for (num in nums) {
         set.add(num)
     }
     val result = IntArray(set.size)
 
-    for ((index, uniqueNumber) in set.withIndex()){
+    for ((index, uniqueNumber) in set.withIndex()) {
         result[index] = uniqueNumber
     }
     return result
 }
 
+fun removeDuplicatesOptimal(nums: Array<Int>):Int{
+    var i = 0;
+    for (j in 1..nums.lastIndex) {
+        if (nums[i] != nums[j]) {
+            nums[i + 1] = nums[j];
+            i++;
+        }
+    }
+    return i + 1;
+}
+
 fun main() {
-    val array = arrayOf(1, 1, 2, 3, 4, 4, 4, 5, 6, 6)
+    val array = arrayOf(1, 1, 2, 2, 2, 3, 3)
     val result = removeDuplicates(array)
     print("Array after removing duplicates is : ")
-    for (i in result){
+    for (i in result) {
         print("$i ")
     }
+
+    println(removeDuplicatesOptimal(nums = array))
 }
