a6ebe79725a89451fac8186a5cccc8609892c0be
swapnil20711 <swapnil.bhojwani@gmail.com>
2025-01-06 23:44:31 +0530

    left rotate by d places optimal

--- arrays/leftRotateArray.kt
+++ arrays/leftRotateArray.kt
@@ -9,11 +9,15 @@ fun leftRotateByOne(nums: Array<Int>): Array<Int> {
     return nums
 }
 
+/*
+* Brute force approach
+* TC : O(n+d) SC : O(d)
+* */
 fun leftRotateByD(nums: Array<Int>, d: Int): Array<Int> {
     val rotateBy = if (d < nums.size) {
         d
     } else {
-         d % nums.size
+        d % nums.size
     }
     val temp = IntArray(rotateBy)
     for (i in 0..<rotateBy) {
@@ -31,12 +35,24 @@ fun leftRotateByD(nums: Array<Int>, d: Int): Array<Int> {
 
 }
 
+fun leftRotateByDOptimal(nums: Array<Int>, d: Int): Array<Int> {
+    val rotateBy = if (d < nums.size) {
+        d
+    } else {
+        d % nums.size
+    }
+    nums.reverse(0, rotateBy)
+    nums.reverse(rotateBy, nums.size)
+    nums.reverse(0, nums.size)
+    return nums
+}
+
 fun main() {
     val array = arrayOf(1, 2, 3, 4, 5)
 //    for (i in leftRotateByOne(array)) {
 //        print("$i ")
 //    }
-    for (i in leftRotateByD(array, 15)) {
+    for (i in leftRotateByDOptimal(array, 18)) {
         print("$i ")
     }
 }
